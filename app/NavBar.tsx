066ca232ead7bcf7abd2973486cebd632bf6c2f8
Tomas Kaiser <tomas.kaiser06@gmail.com>
2025-12-10 18:11:12 +0000

    Fix auto closing hamburger menu when item hit

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,56 +1,109 @@
-import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
-import JHLogo from './components/JHLogo'
-import Link from 'next/link'
+"use client";
+import { faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import JHLogo from "./components/JHLogo";
+import Link from "next/link";
 
 const NavBar = () => {
-    return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
-                <Link href='/' className="mr-1"><JHLogo /></Link>
-                <a
-                    href="https://www.facebook.com/share/jpVp8s9n6sw2aGfd"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn btn-ghost btn-circle"
-                >
-                    <FontAwesomeIcon icon={faFacebook} className="w-6 h-6 text-black" />
-                </a>
-                <a
-                    href="https://www.instagram.com/yenhighjack/?igsh=Yzl0eW1wMGkxN3po&utm_source=qr"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn btn-ghost btn-circle"
-                >
-                    <FontAwesomeIcon icon={faInstagram} className="w-6 h-6 text-black" />
-                </a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <p className='text-3xl font-semibold'>Jan Hajek</p>
-            </div>
-            <div className="navbar-end">
-                <ul className="menu menu-horizontal px-1 hidden lg:flex font-semibold">
-                    <li><Link href={'/albums'}>Albums</Link></li>
-                    <li><Link href={'/about'}>About</Link></li>
-                    <li><Link href={'/contact'}>Contact</Link></li>
-                </ul>
-                <div className="dropdown lg:hidden">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu dropdown-content absolute right-0 bg-base-100 rounded-box mt-3 w-52 p-2 shadow z-50"
-                    >
-                        <li><Link href={'/albums'}>Albums</Link></li>
-                        <li><Link href={'/about'}>About</Link></li>
-                        <li><Link href={'/contact'}>Contact</Link></li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <Link href="/" className="mr-1">
+          <JHLogo />
+        </Link>
+        <a
+          href="https://www.facebook.com/share/jpVp8s9n6sw2aGfd"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-ghost btn-circle"
+        >
+          <FontAwesomeIcon icon={faFacebook} className="w-6 h-6 text-black" />
+        </a>
+        <a
+          href="https://www.instagram.com/yenhighjack/?igsh=Yzl0eW1wMGkxN3po&utm_source=qr"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-ghost btn-circle"
+        >
+          <FontAwesomeIcon icon={faInstagram} className="w-6 h-6 text-black" />
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <p className="text-3xl font-semibold">Jan Hajek</p>
+      </div>
+      <div className="navbar-end">
+        <ul className="menu menu-horizontal px-1 hidden lg:flex font-semibold">
+          <li>
+            <Link href={"/albums"}>Albums</Link>
+          </li>
+          <li>
+            <Link href={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link href={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+        <div className="dropdown lg:hidden">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu dropdown-content absolute right-0 bg-base-100 rounded-box mt-3 w-52 p-2 shadow z-50"
+          >
+            <li>
+              <Link
+                href={"/albums"}
+                onClick={() => {
+                  const el = document.activeElement as HTMLElement | null;
+                  if (el) el.blur();
+                }}
+              >
+                Albums
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/about"}
+                onClick={() => {
+                  const el = document.activeElement as HTMLElement | null;
+                  if (el) el.blur();
+                }}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/contact"}
+                onClick={() => {
+                  const el = document.activeElement as HTMLElement | null;
+                  if (el) el.blur();
+                }}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
